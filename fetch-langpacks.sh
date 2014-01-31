df2cbb123a01548ee92165417ddb41260d8ccf70
Juan Leyva <juanleyvadelgado@gmail.com>
2014-01-31 09:38:16 +0100

    Only search for file recently modified

--- fetch-langpacks.sh
+++ fetch-langpacks.sh
@@ -4,12 +4,18 @@ GIT_PATH="/Users/juanleyvadelgado/Documents/MoodleMobile/moodle-langpacks/moodle
 BRANCH="MOODLE_26_STABLE"
 LANG_DIR="/Users/juanleyvadelgado/Documents/MoodleMobile/GIT/lang"
 # Minimun number of strings translated the language pack should have
-LINES_MIN=170
+LINES_MIN=150
+
+if [ $# -eq 0 ]
+  then
+    echo "Missing file modified time number of days to look for"
+    exit 1
+fi
 
 cd $GIT_PATH
 git checkout $BRANCH
 git pull
-packs=`find . -type f -name "local_moodlemobileapp.php"`
+packs=`find . -type f -name "local_moodlemobileapp.php" -mtime -$1`
 for f in $packs
 do
   lines=`cat $f | grep "string" | wc -l`
@@ -22,5 +28,5 @@ do
       echo "{$file}" | sed "s/,}/}/g"  > $LANG_DIR/$lang.json
       echo "File $lang.json created"
     fi
-  fi 
+  fi
 done
