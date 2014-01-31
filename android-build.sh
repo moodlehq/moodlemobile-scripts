7a52510513a5b34db0f8ff7a36c51f50d190c220
Juan Leyva <juanleyvadelgado@gmail.com>
2014-01-31 09:48:09 +0100

    Added some documentation (comments)

--- android-build.sh
+++ android-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Automatic building system for Android
+# We use and old APK for building, we extrack the apk files, replace the www/files and then package and sign again
+#
+
 DIR_BASE=/Users/juanleyvadelgado/Documents/MoodleMobile/apktool
 GIT_BASE=/Users/juanleyvadelgado/Documents/MoodleMobile/GIT
 KEYSTORE=/Users/juanleyvadelgado/Documents/MoodleMobile/moodlemobile.keystore
@@ -10,11 +15,18 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
+# This directory must exists and also a tmpdir with the Android packacge uncompressed
+# TODO: Improve this to make all the steps automatic
 cd $DIR_BASE/moodlemobile$1
 rm MoodleMobile$1.apk
 rm MoodleMobile$1Store.apk
+# Copy Moodle Mobile files to the Android package
 rsync -a --exclude='.*' $GIT_BASE/ tmpdir/assets/www/
+# Build the apk again
 ../apktool b tmpdir MoodleMobile$1.apk
+# Sign the apk, this prompts for passwords
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE  MoodleMobile$1.apk moodlemobile
+# Compress the apk (mandatory for upload the apk to the Google Play store)
 zipalign -v 4 MoodleMobile$1.apk MoodleMobile$1Store.apk
+# Send by email the new apk, to be tested in a Phone (and for historic also)
 echo "Moodle Mobile apk" | mutt -a "MoodleMobile$1Store.apk" -s "MoodleMobile$1Store.apk" -- $EMAIL
--- fetch-langpacks.sh
+++ fetch-langpacks.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+#
+# Tool for automatic synchronization of language strings translated in AMOS (lang.moodle.net)
+# The strings to be translated are uploaded periodically to the Moodle plugins database via this plugin:
+# https://github.com/jleyva/moodle-local_moodlemobileapp
+#
+
 # GIT FILE PATH
+# http://git.moodle.org/gw?p=moodle-langpacks.git;a=tree;h=refs/heads/MOODLE_26_STABLE;hb=refs/heads/MOODLE_26_STABLE
 GIT_PATH="/Users/juanleyvadelgado/Documents/MoodleMobile/moodle-langpacks/moodle-langpacks"
+# Branch where the translation plugin is placed
 BRANCH="MOODLE_26_STABLE"
+# Directory where Moodle Mobile lang files are located
 LANG_DIR="/Users/juanleyvadelgado/Documents/MoodleMobile/GIT/lang"
 # Minimun number of strings translated the language pack should have
 LINES_MIN=150
@@ -15,6 +24,7 @@ fi
 cd $GIT_PATH
 git checkout $BRANCH
 git pull
+# Lood for language files recently updated
 packs=`find . -type f -name "local_moodlemobileapp.php" -mtime -$1`
 for f in $packs
 do
@@ -24,7 +34,9 @@ do
     lang=`echo $f | sed "s/\.\///" | sed "s/\/.*//"`
     if [ $lang != "en" ]
     then
+      # Convert a Moodle php lang file to a Moodle Mobile json lang file
       file=$(cat $f | grep "\$string" | sed "s/\\\'/'/g" | sed "s/\$string\['/\"/g" | sed "s/'\] = '/\": \"/g" | sed "s/';/\",/g")
+      # Create a valid json file
       echo "{$file}" | sed "s/,}/}/g"  > $LANG_DIR/$lang.json
       echo "File $lang.json created"
     fi
