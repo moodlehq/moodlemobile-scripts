ab29e9d0335a40c47d2181f864a681c85498ffd6
Juan Leyva <juanleyvadelgado@gmail.com>
2014-07-16 10:28:04 +0200

    Conversion tool, from .json lang file to Moodle lang file format

---
+++ json-to-string-lang.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Conversion tool, from .json lang file to Moodle lang file format.
+#2
+
+# JSON file to be converted
+JSON_FILE="/Users/juanleyvadelgado/Documents/MoodleMobile/GIT/lang/en.json"
+# Destination file
+STRING_FILE="/Users/juanleyvadelgado/Documents/MoodleMobile/moodle-local_moodlemobileapp/lang/en/local_moodlemobileapp.php"
+
+header=`cat $STRING_FILE | head -30`
+
+echo "$header" > $STRING_FILE
+
+cat $JSON_FILE | sed "s/'/\\\'/g" | sed 's/"\([^"]*\)" *: *"\([^"]*\)",*/$string[##\1##] = ##\2##;/' \
+| sed "s/##/\'/g" | sed 's/}$//g' | sed 's/^{//g' >> $STRING_FILE
+
+
